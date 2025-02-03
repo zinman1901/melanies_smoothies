45bfd3f3cabc40c2809bc166ce1288f044af03df
zinman1901 <zachinman32@gmail.com>
2025-02-03 12:34:44 -0600

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
