742620a8c2ef7d367266e66915f3b28a422af86b
zinman1901 <zachinman32@gmail.com>
2025-02-03 12:21:29 -0600

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -47,4 +47,5 @@ if ingredients_list:
 #New Section ot dsiiplay smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
