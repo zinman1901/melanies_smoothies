ede51cd0457fb0cfeca1905985086b096dca4324
zinman1901 <zachinman32@gmail.com>
2025-02-03 12:19:29 -0600

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -45,7 +45,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+' '+name_on_order+'!', icon="✅")
 
 #New Section ot dsiiplay smoothiefroot nutrition information
-
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothie_froot_response)
+st.text(smoothiefroot_response)
