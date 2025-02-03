a5980dac14c2df6076e7e4a1b4b9481121ec5251
zinman1901 <zachinman32@gmail.com>
2025-02-03 12:34:08 -0600

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -14,8 +14,9 @@ st.write('The name on your smoothie will be ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 ingredients_list = st.multiselect(
 'Choose Up to 5 ingredients:'
